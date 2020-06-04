630b7f9c10eda48818f45c3945b0deac424214e7
Marcela <mvrivera@unitec.edu>
2020-06-04 13:07:41 -0600

    bien

--- Juego.cpp
+++ Juego.cpp
@@ -1,15 +1,10 @@
 #include "Juego.h"
 
-<<<<<<< HEAD
-
-=======
 /*
 Juego::Juego(){
 	
 }
 */
->>>>>>> edc765a5c8080c7ee9be02da4f7936574e830033
-
 
 void Juego::IniciarPartida(Tablero* tablero, Jugador jugador, Jugador compu){
 	this->tablero = tablero;
--- Juego.h
+++ Juego.h
@@ -4,36 +4,20 @@
 #include "Jugador.h"
 //#include "JugadorHumano.h"
 
-class Juego
-{
-	public:
-<<<<<<< HEAD
-		Juego();
-		void IniciarPartida();
-=======
+class Juego{
+	public:		
 		//Juego();
-		
 		void IniciarPartida(Tablero*, Jugador, Jugador);
->>>>>>> edc765a5c8080c7ee9be02da4f7936574e830033
 		void ValidarGanador();
 		void ElegirGanador();
 		void MostrarPuntuaciones();
 		void ImprimirJugadores();
-<<<<<<< HEAD
-		~Juego();
-=======
-		
+		~Juego();		
 		//~Juego();
->>>>>>> edc765a5c8080c7ee9be02da4f7936574e830033
 	protected:
 		Tablero* tablero;
 		Jugador computadora;
-<<<<<<< HEAD
-		//JugadorHumano jugador;
-=======
-		Jugador jugador;
->>>>>>> edc765a5c8080c7ee9be02da4f7936574e830033
-		
+		Jugador jugador;		
 };
 
 #endif
